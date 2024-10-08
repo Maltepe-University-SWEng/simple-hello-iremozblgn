6b06477d6bee1d1661e0929f07118d0785f9a6c9
irem özbilgin <129671627+iremozblgn@users.noreply.github.com>
2024-10-08 11:47:18 +0300

    sygfuyf

--- module1/src/tr/edu/maltepe/oop/Simple_main.java
+++ module1/src/tr/edu/maltepe/oop/Simple_main.java
@@ -1,10 +1,10 @@
 package tr.edu.maltepe.oop;
-public  class simple_main {
+public  class Simple_main {
 
- public static void main(String []args){
-        //
-       System.out.println("SimpleMain.....");
-        }
+ public static void main(String []args)
+    {
+       System.out.println("benim adım irem");
+    }
 }
 
 
